01d4fd8a7e1521f82c228f59fe1a9818089ab8c0
Kuda11 <kudathomad17@yahoo.com>
2020-11-09 00:09:25 +0000

    socail update

--- src/components/LoginPage/FormLogin/SocialFollow/SocialFollow.jsx
+++ src/components/LoginPage/FormLogin/SocialFollow/SocialFollow.jsx
@@ -10,14 +10,14 @@ import {
 
 const SocialFollow = () => {
 
-  const goToSignInWithGoogle = () => {
-    firebase.auth().signInWithRedirect(provider)
+  const goToSignInWithGoogle = async() => {
+    await firebase.auth().signInWithRedirect(provider)
     navigate("home-page")
   }
   
   return (
     <div className={styles.socialContainer}>
-      <a href="https://github.com/nology-tech/dada" onClick={goToSignInWithGoogle} className={styles.twitterSocial}>
+      <a href="" onClick={goToSignInWithGoogle} className={styles.twitterSocial}>
         <FontAwesomeIcon icon={faGoogle} size="3x" />
       </a>
     </div>
